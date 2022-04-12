a52d25baf7a147edb88d2032335e03654fc7d255
Jonas Augusto <jonasaugusto99@gmail.com>
2022-04-12 13:57:46 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function positions(firstPlace, secondPlace, lastPlace){
+
+    let position = [firstPlace, secondPlace, lastPlace]
+    
+    if(lastPlace == "Daniel"){
+        position[2] = secondPlace
+        position[1] = lastPlace
+    }else if(secondPlace == "Daniel"){
+        position[1] = firstPlace
+        position[0] = secondPlace
+    }
+    return `1° - Colocado ${position[0]},
+            2° - Colocado ${position[1]}, 
+            3° - Colocado ${position[2]}`
+    
+}
+
+console.log(positions("Manoel", "Rafael", "Daniel"))
